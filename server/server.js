ed3bf02c80427d19491ee4fe346b04feb0366915
interdigitize <interdigitize@users.noreply.github.com>
2017-04-20 07:33:06 -0700

    update the server.js: nests  the readFile function in fs.stat so it won't be called until after the file info object knows if the file path points to a directory. Hopefully a solution to the Heroku 500 Error, EISDIR: illegal operation on a directory, read.

--- server/server.js
+++ server/server.js
@@ -2,33 +2,32 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const server = http.createServer(function(request, response){
-  const urlPath = url.parse(request.url).pathname;
-  let filePath = `./client/${urlPath}`;
+const server = http.createServer(function(request, response) {
+    const urlPath = url.parse(request.url).pathname;
+    let filePath = `./client/${urlPath}`;
+    fs.stat(filePath, function(err, fileInfo) {
+        if (err) {
+            response.statusCode = 404;
+            response.end(`Resource not found: "${urlPath}"`);
+        } else {
 
-  fs.stat(filePath, function(err, fileInfo){
-    if(!err && fileInfo.isDirectory()){
-      filePath += '/index.html';
-    }
-  });
-  fs.exists(filePath, function(doesExist){
-    if (!doesExist){
-      response.statusCode = 404;
-      response.end(`Resource not found: "${urlPath}"`);
-    }
-  });
+            if (fileInfo.isDirectory()) {
+                filePath += '/index.html';
+            }
 
-
-  fs.readFile(filePath, function(err, data) {
-    if (err){
-      response.statusCode = 500;
-      response.end(`Server error: "${err}"`);
-    } else {
-      response.end(data.toString('UTF-8'));
-    }
-  });
+            fs.readFile(filePath, function(err, data) {
+                if (err) {
+                    response.statusCode = 500;
+                    response.end(`Server error: "${err}"`);
+                } else {
+                    response.end(data.toString('utf-8'));
+                }
+            });
+        }
+    });
 });
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, function(){
-  console.log(`Server listening on port ${PORT}...`);
+server.listen(PORT, function() {
+    console.log(`Server listening on port ${PORT}...`);
 });
